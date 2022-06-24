3eb81adeeaddbdcba07e31ec96e16b9c4d77a272
a.chepiha <a.chepiha@it4medicine.com.ua>
2022-06-25 00:45:24 +0300

    init

---
+++ DataLayer/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WMS.DataLayer.Models;
+
+namespace WMS.DataLayer
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Slot> Slots { get; set; }
+        public DbSet<Supply> Supplies { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
